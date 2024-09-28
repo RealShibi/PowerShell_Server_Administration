0bd8a171e181778a8aa92257aee5362a9a5afcc1
Shibi <shibi@shibisweb.de>
2024-09-28 10:11:00 +0200

    Created Template Script for pwsh7

---
+++ Template_pwsh.ps1
@@ -0,0 +1,85 @@
+﻿<#
+    .SYNOPSIS
+    PUT SHORT SCRIPT DESCRIPTION HERE AND ADD ANY ADDITIONAL KEYWORD SECTIONS AS NEEDED (.PARAMETER, .EXAMPLE, ETC.).
+
+    .DESCRIPTION
+    Provide a more detailed description of what the script does here.
+
+    .PARAMETER ParameterName
+    Description of the parameter.
+
+    .EXAMPLE
+    Example of how to use this script.
+
+    .NOTES
+    Author: Your Name
+    Date: YYYY-MM-DD
+    Version: 1.0
+#>
+[CmdletBinding()]
+param (
+    # Define parameters here and delete this comment.
+    [Parameter(Mandatory = $false)]
+    [string]$csvpath = "\output.csv",
+    [Parameter(Mandatory = $false)]
+    [string]$encoding = "UTF8",
+    [Parameter(Mandatory = $false)]
+    [string]$delimiter = ";"
+)
+
+begin {
+    # DEFINE FUNCTIONS HERE AND DELETE THIS COMMENT.
+
+    $InformationPreference = 'Continue'
+    # $VerbosePreference = 'Continue' # Uncomment this line if you want to see verbose messages.
+
+    # get date in format yyyy-MM-dd-HH-mm-ss
+    $date = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+
+    # Log all script output to a file for easy reference later if needed.
+    [string] $lastRunLogFilePath = "$PSCommandPath.$date.LastRun.log"
+    Start-Transcript -Path $lastRunLogFilePath
+
+    # Display the time that this script started running.
+    [DateTime] $startTime = Get-Date
+    Write-Information "Starting script at '$($startTime.ToString('u'))'."
+
+    # Function to handle errors
+    function Handle-Error {
+        param (
+            [string]$Message
+        )
+        Write-Error $Message
+        Stop-Transcript
+        throw $Message
+    }
+
+    # Function to log information
+    function Log-Information {
+        param (
+            [string]$Message
+        )
+        Write-Information $Message
+    }
+}
+
+process {
+
+    try {
+        Log-Information "Processing..."
+        # Code Here
+
+    }
+    catch {
+        Handle-Error $_.Exception.Message
+    }
+}
+
+end {
+    # Display the time that this script finished running, and how long it took to run.
+    [DateTime] $finishTime = Get-Date
+    [TimeSpan] $elapsedTime = $finishTime - $startTime
+    Write-Information "Finished script at '$($finishTime.ToString('u'))'. Took '$elapsedTime' to run."
+
+    Stop-Transcript
+}
